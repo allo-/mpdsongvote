8b0f87e50a1c7f47306de6f49d70f36e5db891ba
Alexander Schier <allo@laxu.de>
2014-12-21 00:31:08 +0100

    script to remove played songs from playlist and add new ones

---
+++ update_playlist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import random
+import mpd
+import mpdsongvote.models
+
+PLAYLIST_LENGTH = 100  # desired playlist length after removing/adding songs
+
+def main_print(main, args):
+    if main:
+        print args
+
+def update_playlist(main=False):
+    c = mpd.MPDClient()
+    c.connect("localhost", 6600)
+
+    # 1) remove played songs from the beginning
+    playlist_songs_by_pos = dict(map(lambda x: (int(x['pos']), x), c.playlistid()))
+    current_song_no = int(c.status()['song'])
+
+    main_print(main, "removing:")
+    for i in xrange(current_song_no):
+        main_print(main, playlist_songs_by_pos[0]['file'])
+        c.delete(0)
+
+    # 2) add new songs at the bottom
+    playlist_songs = dict(map(lambda x: (x['file'], x), c.playlistid()))
+    all_songs = dict(map(lambda x: (x['file'], x), c.search("file", "")))
+
+    new_files = list(set(all_songs).difference(playlist_songs))
+    new_files = new_files[0:max(0, PLAYLIST_LENGTH - len(playlist_songs))]
+    random.shuffle(new_files)
+
+    main_print(main, "adding:")
+    for thefile in new_files:
+        main_print(main, thefile)
+        c.add(thefile)
+
+    c.disconnect()
+
+if __name__ == "__main__":
+    import django
+    django.setup()
+    update_playlist(main=True)
