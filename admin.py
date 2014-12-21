1b6cdb1195ccae07a47a6b8ad165fb43e4aed9a5
Alexander Schier <allo@laxu.de>
2014-12-21 18:49:41 +0100

    added more list columns in the admin interface

--- admin.py
+++ admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 import models
 
-admin.site.register(models.PlaylistItem)
-admin.site.register(models.PlaylistVote)
-admin.site.register(models.Exclude)
-admin.site.register(models.PlayedSong)
+
+class PlaylistItemAdmin(admin.ModelAdmin):
+    list_display = ("filename", )
+
+
+def playlistitem_filename(playlistvote):
+    return playlistvote.playlistitem.filename
+playlistitem_filename.short_description = "filename"
+
+
+class PlaylistVoteAdmin(admin.ModelAdmin):
+    list_display = (playlistitem_filename, "value")
+
+
+class ExcludeAdmin(admin.ModelAdmin):
+    list_display = ("value", "field")
+
+
+class PlayedSongAdmin(admin.ModelAdmin):
+    list_display = ("title", "artist", "filename")
+
+
+admin.site.register(models.PlaylistItem, PlaylistItemAdmin)
+admin.site.register(models.PlaylistVote, PlaylistVoteAdmin)
+admin.site.register(models.Exclude, ExcludeAdmin)
+admin.site.register(models.PlayedSong, PlayedSongAdmin)
