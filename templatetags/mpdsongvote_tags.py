50c54a41580ecf371a008846a7f5d93bcc16997d
Alexander Schier <allo@laxu.de>
2014-12-23 14:04:47 +0100

    reconstruct title from filename, if the file has no title metadata

--- templatetags/mpdsongvote_tags.py
+++ templatetags/mpdsongvote_tags.py
@@ -10,3 +10,15 @@ def divmod_filter(value, arg):
 @register.filter(name="zfill")
 def zfill_filter(value, arg):
     return str(value).zfill(arg)
+
+
+@register.filter(name="title_from_filename")
+def title_from_filename_filter(value):
+    # remove path
+    filename = value.split("/")[-1]
+    # remove file extension
+    parts = filename.split(".")
+    if len(parts) == 1:
+        return parts[0]
+    else:
+        return ".".join(parts[:-1])
